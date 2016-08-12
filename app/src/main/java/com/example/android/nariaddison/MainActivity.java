3c692f047999f1bc3f5c242b8cb2734586397ea8
Andreas Vieten <andreasvieten@yahoo.de>
2016-08-12 14:36:02 +0200

    Checkboxes für Medizingaben + Datum hinzugefügt

--- app/src/main/java/com/example/android/nariaddison/MainActivity.java
+++ app/src/main/java/com/example/android/nariaddison/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.nariaddison;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
 
